92afc55a74145bf1013fef82cb568b39467ffeec
Daniel X Moore <yahivin@gmail.com>
2013-10-07 12:59:03 -0700

    :clock12::clock330: Updated in browser at strd6.github.io/editor

---
+++ v0.1.0.js
@@ -0,0 +1,24 @@
+(function() {
+  module.exports = function(_arg) {
+    var height, methods, name, sandbox, width, _ref;
+    _ref = _arg != null ? _arg : {}, name = _ref.name, width = _ref.width, height = _ref.height, methods = _ref.methods;
+    if (name == null) {
+      name = "sandbox" + new Date;
+    }
+    if (width == null) {
+      width = 800;
+    }
+    if (height == null) {
+      height = 600;
+    }
+    if (methods == null) {
+      methods = {};
+    }
+    sandbox = window.open("", name, "width=" + width + ",height=" + height);
+    Object.extend(sandbox, methods);
+    return sandbox;
+  };
+
+}).call(this);
+
+//# sourceURL=main.coffee
